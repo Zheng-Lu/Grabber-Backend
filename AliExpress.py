a21b2111416d17b32a0bdb583ddd327cb89994dd
Zheng-Lu <s2085615@ed.ac.uk>
2021-08-17 16:46:54 +0800

    Successfully bypassed the captcha page

--- AliExpress.py
+++ AliExpress.py
@@ -1,11 +1,7 @@
 import os
-import re
+import execjs
 import requests
 import random
-import json
-import time
-import pandas as pd
-from selenium.webdriver.support.wait import WebDriverWait
 from bs4 import BeautifulSoup
 
 user_agent_list = [
@@ -16,17 +12,62 @@ user_agent_list = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
 ]
 
-
 AliExpress_headers = {
     'user-agent': random.choice(user_agent_list),
 }
 
-def downloadSourceCode(url):
-    """
+payload = {
+    "fm-login-id": 'Lz429671594@outlook.com',
+    'fm-login-password': 'pf52CE4JyGiY7fj'
+}
 
+
+def js_from_file(file_name):
+    """
+    reading javascript file
+    :param file_name:
     :return:
     """
+    with open(file_name, 'r', encoding='UTF-8') as file:
+        result = file.read()
+
+    return result
+
+
+js_code = execjs.compile(js_from_file(r"D:\App Development\Grabber\AliSliderCracker.js"))
+
+
+def downloadSourceCode():
+    store_id = int(input('Store ID to grab source from: '))
+    numPages = int(input('Number of pages you want to scrape: '))
+    path = input('The path you want to save the file: ')
+    html_output_name = input('Name for html file: ')
+
+    urls = []
+    reqs = []
+
+    for i in range(1, numPages + 1):
+        urls.append("https://www.aliexpress.com/store/sale-items/" + str(store_id) + '/' + str(i) + ".html")
+
+    for url in urls:
+        req_result = requests.get(url, 'html.parser').text
+        if 'pic-rind' not in req_result:
+            js_code.call("bypass", url)
+        else:
+            reqs.append(req_result)
+
+    with open(path + html_output_name + '.txt', 'w', encoding='gb18030', errors='ignore') as f:
+        for req in reqs:
+            f.write(req + "\n")
+        f.close()
+
+
 def getProductURL(list_dir):
+    """
+    :param list_dir:
+    :return:
+    """
+
     path = list_dir + '//'
     htmlDirEntries = sorted(
         [filename for filename in os.listdir(path) if filename.endswith(".html")])
@@ -50,6 +91,10 @@ def download_product(HTMLdict):
         for url in HTMLdict[filename]:
             response = requests.get(url, headers=AliExpress_headers)
             content = response.content
-            id = (url.split('.html')[0]).split('/item/')[1]
-            with open("C://Users//Lenovo//Desktop//html//911651019//" + id + '.html', 'wb') as f:
+            product_id = (url.split('.html')[0]).split('/item/')[1]
+            with open("C://Users//Lenovo//Desktop//html//911651019//" + product_id + '.html', 'wb') as f:
                 f.write(content)
+
+
+if __name__ == '__main__':
+    downloadSourceCode()
